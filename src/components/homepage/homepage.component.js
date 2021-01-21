c328c2ceb841b29a0ca1a944a128c87bd03b707d
James Vongampai <jaythomasv29@gmail.com>
2021-01-21 08:51:41 -0800

    separate app component into various components

---
+++ src/components/homepage/homepage.component.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Directory from '../directory/directory.component'
+import "./homepage.styles.scss";
+
+function HomePage() {
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  );
+}
+
+export default HomePage;
