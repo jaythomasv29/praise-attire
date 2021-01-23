7acdbb138bc7d422e5d84431b697b36d5e7e4b3e
James Vongampai <jaythomasv29@gmail.com>
2021-01-23 04:57:23 -0800

    add header component to show various routes with css

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import './App.css';
 import { Switch, Route } from 'react-router-dom'
 import HomePage from './components/homepage/homepage.component'
 import Shop from './components/shop/shop.component'
+import Header from './components/header/header.component'
 
 function App() {
   return (
     <div>
+    <Header />
     <Switch>
     <Route  exact path='/' component={ HomePage } />
     <Route  path='/shop' component={ Shop } />
-    
     </Switch>
     </div>
   );
